5d4f6607be827dbdcac9925d7ce2532be9d4d517
SimRey <simonereynoso@gmail.com>
2022-11-12 17:07:35 -0600

    ok

--- Pascal Poupart/Assignment 1/Part 3/Manual DQN with TF2/replay_buffer.py
+++ Pascal Poupart/Assignment 1/Part 3/Manual DQN with TF2/replay_buffer.py
@@ -1,36 +1,36 @@
 import numpy as np
+import tensorflow as tf
 
 class ReplayBuffer(object):
-    def __init__(self, max_size, input_dims):
-        self.mem_size = max_size       # Memory size of the Replay Buffer
-        self.mem_cntr = 0              # Memory counter
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape),
+                                     dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape),
+                                         dtype=np.float32)
 
-        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int64)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.int32) # This terminal memory is used to avoid counting an extra reward when the process has finished
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
 
-    
     def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.mem_size  # Once the replay buffer is full, the index restarts as zero, otherwise is the index
-
+        index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         self.new_state_memory[index] = state_
         self.action_memory[index] = action
         self.reward_memory[index] = reward
-        self.terminal_memory[index] = 1 - int(done)   # In this case True = 0, False = 1, so terminal memory has to be encoded in this way
-
+        self.terminal_memory[index] = done
         self.mem_cntr += 1
 
     def sample_buffer(self, batch_size):
         max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, batch_size, replace=False) # repalce = False --> taken out of the pool
+        batch = np.random.choice(max_mem, batch_size, replace=False)
 
-        states = self.state_memory[batch]
-        states_ = self.new_state_memory[batch]
-        action = self.action_memory[batch]
-        reward = self.reward_memory[batch]
-        terminal = self.terminal_memory[batch]
+        states = tf.convert_to_tensor(self.state_memory[batch])
+        actions = tf.convert_to_tensor(self.action_memory[batch], dtype=np.int32)
+        rewards = tf.convert_to_tensor(self.reward_memory[batch])
+        states_ = tf.convert_to_tensor(self.new_state_memory[batch])
+        terminal = tf.convert_to_tensor(self.terminal_memory[batch])
 
-        return states, states_, action, reward, terminal
+        return states, actions, rewards, states_, terminal
