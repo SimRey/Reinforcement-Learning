b35a34cddd9a2fe0d41fd566906e2a364fa20efc
SimRey <simonereynoso@gmail.com>
2023-01-26 17:26:29 -0500

    ok

--- 4. Policy gradient/DDPG/Pytorch/networks.py
+++ 4. Policy gradient/DDPG/Pytorch/networks.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 
 class CriticNetwork(nn.Module):
     def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions, name,
-                 chkpt_dir='tmp/ddpg'):
+                 chkpt_dir='ddpg/'):
         super(CriticNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -17,38 +17,21 @@ class CriticNetwork(nn.Module):
         self.checkpoint_file = os.path.join(chkpt_dir, name)
         
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-
-        # Initializing weights and biases of the layer --> get better convergence
-        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])   
-        self.fc1.weight.data.uniform_(-f1, f1)
-        self.fc1.bias.data.uniform_(-f1, f1)
-        self.bn1 = nn.LayerNorm(self.fc1_dims)
-
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
-        self.fc2.weight.data.uniform_(-f2, f2)
-        self.fc2.bias.data.uniform_(-f2, f2)
-        self.bn2 = nn.LayerNorm(self.fc2_dims)
-
-        self.action_value = nn.Linear(self.n_actions, self.fc2_dims)
-        f3 = 0.003
+        
+        self.action_value = nn.Linear(self.n_actions, self.fc1_dims)
         self.q = nn.Linear(self.fc2_dims, 1)
-        self.q.weight.data.uniform_(-f3, f3)
-        self.q.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
 
 
     def forward(self, state, action):
         state_value = self.fc1(state)
-        state_value = self.bn1(state_value)
-        state_value = F.relu(state_value)
-
-        state_value = self.fc2(state_value)
-        state_value = self.bn2(state_value)
-
         action_value = F.relu(self.action_value(action))
         state_action_value = F.relu(T.add(state_value, action_value))
+    
+        state_action_value = self.fc2(state_action_value)
+        state_action_value = F.relu(state_action_value)
         state_action_value = self.q(state_action_value)
 
         return state_action_value
@@ -65,7 +48,7 @@ class CriticNetwork(nn.Module):
 
 class ActorNetwork(nn.Module):
     def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name,
-                 chkpt_dir='tmp/ddpg'):
+                 chkpt_dir='ddpg/'):
         super(ActorNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -74,31 +57,16 @@ class ActorNetwork(nn.Module):
         self.checkpoint_file = os.path.join(chkpt_dir, name)
 
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
-        self.fc1.weight.data.uniform_(-f1, f1)
-        self.fc1.bias.data.uniform_(-f1, f1)
-        self.bn1 = nn.LayerNorm(self.fc1_dims)
-
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
-        self.fc2.weight.data.uniform_(-f2, f2)
-        self.fc2.bias.data.uniform_(-f2, f2)
-        self.bn2 = nn.LayerNorm(self.fc2_dims)
-
-        f3 = 0.003
         self.mu = nn.Linear(self.fc2_dims, self.n_actions)
-        self.mu.weight.data.uniform_(-f3, f3)
-        self.mu.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
         
 
     def forward(self, state):
         x = self.fc1(state)
-        x = self.bn1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        x = self.bn2(x)
         x = F.relu(x)
         x = T.tanh(self.mu(x))
 
