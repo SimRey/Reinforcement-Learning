7cbb7ccc5e2f78e19f74141d216811d3d5619944
SimRey <simonereynoso@gmail.com>
2023-02-01 17:32:07 -0500

    ok

--- 5. PPO/memory.py
+++ 5. PPO/memory.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class PPOMemory:
-    def __init__(self, batch_size, T):
+    def __init__(self, batch_size):
         self.states = []
         self.probs = []
         self.actions = []
@@ -10,18 +10,21 @@ class PPOMemory:
         self.dones = []
         self.new_states = []
 
-        self.n_states = T
         self.batch_size = batch_size
 
     def recall(self):
-        # Returns the lists as numpy arrays
-        return np.array(self.states), np.array(self.new_states), np.array(self.actions),\
-            np.array(self.probs), np.array(self.rewards), np.array(self.dones)
+        return np.array(self.states),\
+            np.array(self.new_states),\
+            np.array(self.actions),\
+            np.array(self.probs),\
+            np.array(self.rewards),\
+            np.array(self.dones)
 
     def generate_batches(self):
+        n_states = len(self.states)
         # batch_start = np.arange(0, n_states, self.batch_size)
-        n_batches = int(self.n_states // self.batch_size)
-        indices = np.arange(self.n_states, dtype=np.int64)
+        n_batches = int(n_states // self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
         np.random.shuffle(indices)
         # batches = [indices[i:i+self.batch_size] for i in batch_start]
         batches = [indices[i*self.batch_size:(i+1)*self.batch_size]
