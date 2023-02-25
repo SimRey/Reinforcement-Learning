8dd7a03fdbb3b9b3d4e285180305597f435c0231
SimRey <simonereynoso@gmail.com>
2023-02-25 17:09:14 -0500

    ok

---
+++ 5. PPO/Hybrid PPO/agent.py
@@ -0,0 +1,159 @@
+import torch as T
+from memory import PPOMemory
+from networks import HybridActorNetwork, HybridCriticNetwork
+
+
+class Agent:
+    def __init__(self, actions, input_dims, fc1_dims=128, fc2_dims=128, gamma=0.99, alpha=3e-4,
+                 gae_lambda=0.95, policy_clip=0.2, batch_size=64, n_epochs=10):
+        
+        self.actions = actions
+        self.input_dims = input_dims
+        self.alpha = alpha
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.gamma = gamma
+        self.policy_clip = policy_clip
+        self.n_epochs = n_epochs
+        self.gae_lambda = gae_lambda
+        self.batch_size = batch_size
+        
+        self.actor = HybridActorNetwork(self.actions, self.input_dims, 
+            self.alpha, self.fc1_dims, self.fc2_dims)
+        self.critic = HybridCriticNetwork(self.input_dims, self.alpha, 
+            self.fc1_dims, self.fc2_dims)
+
+        self.memory = PPOMemory(self.batch_size)
+
+    def remember(self, state, state_, action_d, action_c, probs_d, probs_c, reward, done):
+        self.memory.store_memory(state, state_, action_d, action_c, probs_d, probs_c, reward, done)
+
+    def save_models(self):
+        self.actor.save_checkpoint()
+        self.critic.save_checkpoint()
+
+    def load_models(self):
+        self.actor.load_checkpoint()
+        self.critic.load_checkpoint()
+
+    def choose_action(self, observation):
+        with T.no_grad():
+            state = T.tensor([observation], dtype=T.float)
+
+            dist_d, dist_c = self.actor(state)
+
+            # Discrete
+            action_d = dist_d.sample()
+            probs_d = T.squeeze(dist.log_prob(action_d)).item()
+            action_d = T.squeeze(action_d).item()
+
+            # Continuous
+            action_c = dist_c.sample()
+            probs_c = dist.log_prob(action_c)
+            action_c = action_c.numpy().flatten()
+            probs_c = probs_c.numpy().flatten()
+
+            return action_d, probs_d, action_c, probs_c
+
+
+    def calc_adv_and_returns(self, memories):
+        states, new_states, r, dones = memories
+        with T.no_grad():
+            values = self.critic(states)
+            values_ = self.critic(new_states)
+            deltas = r + self.gamma * values_ - values
+            deltas = deltas.flatten().numpy()
+            adv = [0]
+            for step in reversed(range(deltas.shape[0])):
+                advantage = deltas[step] + self.gamma * self.gae_lambda * adv[-1] * (1 - dones[step])
+                adv.append(advantage)
+            adv.reverse()
+            adv = adv[:-1]
+            adv = T.tensor(adv).float().unsqueeze(1)
+            returns = adv + values
+            adv = (adv - adv.mean()) / (adv.std()+1e-6)
+        return adv, returns
+
+    
+    def learn(self):
+        state_arr, new_state_arr, actions_d_arr, actions_c_arr, \
+            old_prob_d_arr, old_prob_c_arr, reward_arr, dones_arr = self.memory.recall()
+        
+        state_arr = T.tensor(state_arr, dtype=T.float)
+        new_state_arr = T.tensor(new_state_arr, dtype=T.float)
+        actions_d_arr = T.tensor(actions_d_arr, dtype=T.float)
+        actions_c_arr = T.tensor(actions_c_arr, dtype=T.float)        
+        old_prob_d_arr = T.tensor(old_prob_d_arr, dtype=T.float)
+        old_prob_c_arr = T.tensor(old_prob_c_arr, dtype=T.float)
+        r = T.tensor(reward_arr, dtype=T.float).unsqueeze(1)
+
+        adv, returns = self.calc_adv_and_returns((state_arr, new_state_arr, r, dones_arr))
+
+
+        for epoch in range(self.n_epochs):
+            batches = self.memory.generate_batches()
+            total_params = len(self.actor.parameters())
+            for batch in batches:
+                states = state_arr[batch]
+                
+                # Discrete update
+                old_probs = old_prob_d_arr[batch]
+                actions = actions_d_arr[batch]
+
+                dist_d, _ = self.actor(states)
+                new_probs = dist.log_prob(actions)
+                prob_ratio = T.exp(new_probs.sum(1, keepdim=True) - old_probs.sum(1, keepdim=True))
+                weighted_probs = adv[batch] * prob_ratio
+                weighted_clipped_probs = T.clamp(prob_ratio, 1-self.policy_clip, 1+self.policy_clip) * adv[batch]
+                actor_loss = -T.min(weighted_probs, weighted_clipped_probs)
+
+                # Deactivate continuouse parameters
+                for param in reversed(self.actor.parameters()):
+                    for i in range(4):
+                        param.requires_grad = False
+
+                self.actor.optimizer.zero_grad()
+                actor_loss.mean().backward()
+                self.actor.optimizer.step()
+
+                # Activate all parameters again
+                for param in self.actor.parameters():
+                    param.requires_grad = True
+
+                
+                # Continuous update
+                old_probs = old_prob_c_arr[batch]
+                actions = actions_c_arr[batch]
+
+                _, dist_c = self.actor(states)
+                new_probs = dist.log_prob(actions)
+                prob_ratio = T.exp(new_probs.sum(1, keepdim=True) - old_probs.sum(1, keepdim=True))
+                weighted_probs = adv[batch] * prob_ratio
+                weighted_clipped_probs = T.clamp(prob_ratio, 1-self.policy_clip, 1+self.policy_clip) * adv[batch]
+                actor_loss = -T.min(weighted_probs, weighted_clipped_probs)
+
+                # Deactivate discrete parameters
+                for param in reversed(self.actor.parameters()):
+                    for i in range(6):
+                        if i < 4:
+                            continue
+                        else:
+                            param.requires_grad = False
+
+                self.actor.optimizer.zero_grad()
+                actor_loss.mean().backward()
+                self.actor.optimizer.step()
+
+                # Activate all parameters again
+                for param in self.actor.parameters():
+                    param.requires_grad = True
+
+
+                # Critic network update
+                critic_value = self.critic(states)
+                critic_loss = (critic_value - returns[batch]).pow(2).mean()
+                self.critic.optimizer.zero_grad()
+                critic_loss.backward()
+                self.critic.optimizer.step()
+        
+        self.memory.clear_memory()
---
+++ 5. PPO/Hybrid PPO/networks.py
@@ -0,0 +1,87 @@
+import os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Beta, Categorical
+
+
+class HybridActorNetwork(nn.Module):
+    def __init__(self, actions, input_dims, alpha,
+                 fc1_dims, fc2_dims, chkpt_dir='models/'):
+        super(HybridActorNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir,'hppo')
+        self.input_dims = input_dims
+        self.alpha = alpha
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        
+        self.actions = actions
+        # Discrete actions
+        self.d_actions = self.actions["discrete"].n
+        # Continuous actions
+        self.c_actions = self.actions["discrete"].shape[0]
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        # Discrete actions
+        self.pi = nn.Linear(self.fc2_dims, self.d_actions)
+        # Continuous actions
+        self.alpha = nn.Linear(self.fc2_dims, self.c_actions)
+        self.beta = nn.Linear(self.fc2_dims, self.c_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=self.alpha)
+       
+
+    def forward(self, state):
+        x = T.tanh(self.fc1(state))
+        x = T.tanh(self.fc2(x))
+        
+        # Discrete distribution
+        pi = F.softmax(self.pi(x), dim=1)
+        dist_d = Categorical(pi)
+
+        # Continuous distribution
+        alpha = F.relu(self.alpha(x)) + 1.0
+        beta = F.relu(self.beta(x)) + 1.0
+        dist_c = Beta(alpha, beta)
+
+        return dist_d, dist_c
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+
+class HybridCriticNetwork(nn.Module):
+    def __init__(self, input_dims, alpha,
+                 fc1_dims, fc2_dims, chkpt_dir='models/'):
+        super(HybridCriticNetwork, self).__init__()
+        self.checkpoint_file = os.path.join(chkpt_dir,'critic_continuous_ppo')
+        self.input_dims = input_dims
+        self.alpha = alpha
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.v = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=self.alpha)
+
+    def forward(self, state):
+        x = T.tanh(self.fc1(state))
+        x = T.tanh(self.fc2(x))
+        v = self.v(x)
+
+        return v
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
